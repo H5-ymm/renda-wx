eb3ddb722739be1deb8a205c19ed39cc540fc5bd
ymm <ymmwebh5@163.com>
2020-02-01 00:11:30 +0800

    接口

--- src/util.js
+++ src/util.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-array-constructor */
 const manglingFormatCardNumber = (cardNumber) => {
     if (cardNumber && cardNumber.length > 8) {
         return `${cardNumber.substring(0, 4)} ${'*'
@@ -10,6 +11,73 @@ const manglingFormatCardNumber = (cardNumber) => {
     // eslint-disable-next-line semi
     return cardNumber
 };
+const validateIdCard = idCard => {
+    // 15位和18位身份证号码的正则表达式
+    var regIdCard = /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/;
+    var flag = false;
+    // 如果通过该验证，说明身份证格式正确，但准确性还需计算
+    if (regIdCard.test(idCard)) {
+        if (idCard.length === 18) {
+            // eslint-disable-next-line no-array-constructor
+            var idCardWi = new Array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2); // 将前17位加权因子保存在数组里
+            var idCardY = new Array(1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2); // 这是除以11后，可能产生的11位余数、验证码，也保存成数组
+            var idCardWiSum = 0; // 用来保存前17位各自乖以加权因子后的总和
+            for (var i = 0; i < 17; i++) {
+                idCardWiSum += idCard.substring(i, i + 1) * idCardWi[i];
+            }
+            var idCardMod = idCardWiSum % 11; // 计算出校验码所在数组的位置
+            var idCardLast = idCard.substring(17); // 得到最后一位身份证号码
+            // 如果等于2，则说明校验码是10，身份证号码最后一位应该是X
+            if (idCardMod === 2) {
+                if (idCardLast === 'X' || idCardLast === 'x') {
+                    flag = true;
+                } else {
+                    flag = false;
+                }
+            } else {
+                // 用计算出的验证码与最后一位身份证号码匹配，如果一致，说明通过，否则是无效的身份证号码
+                if (idCardLast === idCardY[idCardMod]) {
+                    flag = true;
+                } else {
+                    flag = false;
+                }
+            }
+        }
+    } else {
+        flag = false;
+    }
+    return flag;
+};
+
+const checkMobile = mobile => {
+    let reg = /^1[3456789]\d{9}$/;
+    let flag = false;
+    if (!reg.test(mobile)) {
+        flag = false;
+    } else {
+        flag = true;
+    }
+    return flag;
+};
+const changeFormat = format => {
+    let time = new Date();
+    var date = {
+        'M+': time.getMonth() + 1, // 月份
+        'd+': time.getDate(), // 日
+    };
+    if (/(y+)/i.test(format)) {
+        format = format.replace(RegExp.$1, (time.getFullYear() + '').substr(4 - RegExp.$1.length));
+    }
+    for (var k in date) {
+        if (new RegExp('(' + k + ')').test(format)) {
+            format = format.replace(RegExp.$1, RegExp.$1.length === 1 ? date[k] : ('00' + date[k]).substr(('' + date[k]).length));
+        }
+    };
+    return format;
+};
 module.exports = {
-    manglingFormatCardNumber: manglingFormatCardNumber
+    manglingFormatCardNumber: manglingFormatCardNumber,
+    validateIdCard: validateIdCard,
+    checkMobile: checkMobile,
+    changeFormat: changeFormat
 };
