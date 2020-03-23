51533b255fb225f6d53cab55c8ff67b56a127aaa
H5-ymm <1594363841@qq.com>
2020-03-24 01:11:36 +0800

    change

--- src/base/base.js
+++ src/base/base.js
@@ -1,80 +1,88 @@
 export const list1 = [{
-    pagePath: '/pages/companyView/index',
-    text: '发单管理',
-    selected: true,
-    iconPath: '/images/orderTaking/list.png',
-    selectedIconPath: '/images/orderTaking/list-active.png'
-},
-{
-    pagePath: '/pages/companyView/staffManage',
-    text: '员工管理',
-    selected: false,
-    iconPath: '/images/orderTaking/staff.png',
-    selectedIconPath: '/images/orderTaking/staff-active.png'
-},
-{
-    pagePath: '/pages/my/index',
-    text: '企业中心',
-    selected: false,
-    iconPath: '/images/orderTaking/my.png',
-    selectedIconPath: '/images/orderTaking/my-active.png'
-}
+        pagePath: '/pages/companyView/index',
+        text: '发单管理',
+        selected: true,
+        iconPath: '/images/orderTaking/list.png',
+        selectedIconPath: '/images/orderTaking/list-active.png'
+    },
+    {
+        pagePath: '/pages/companyView/staffManage',
+        text: '员工管理',
+        selected: false,
+        iconPath: '/images/orderTaking/staff.png',
+        selectedIconPath: '/images/orderTaking/staff-active.png'
+    },
+    {
+        pagePath: '/pages/message/index',
+        text: '消息管理',
+        selected: false,
+        iconPath: '/images/orderTaking/staff.png',
+        selectedIconPath: '/images/orderTaking/staff-active.png'
+    },
+
+    {
+        pagePath: '/pages/my/index',
+        text: '企业中心',
+        selected: false,
+        iconPath: '/images/orderTaking/my.png',
+        selectedIconPath: '/images/orderTaking/my-active.png'
+    }
 ]
 export const list2 = [{
-    pagePath: '/pages/teamView/index',
-    text: '首页',
-    selected: true,
-    iconPath: '/images/team/home.png',
-    selectedIconPath: '/images/team/home-active.png'
-},
-{
-    pagePath: '/pages/my/index',
-    text: '我的',
-    selected: false,
-    iconPath: '/images/my.png',
-    selectedIconPath: '/images/my-active.png'
-}
+        pagePath: '/pages/teamView/index',
+        text: '首页',
+        selected: true,
+        iconPath: '/images/team/home.png',
+        selectedIconPath: '/images/team/home-active.png'
+    },
+    {
+        pagePath: '/pages/my/index',
+        text: '我的',
+        selected: false,
+        iconPath: '/images/my.png',
+        selectedIconPath: '/images/my-active.png'
+    }
 ]
 export const welfareList = [{
-    label: '是',
-    value: 1
-}, {
-    label: '否',
-    value: 2
-},
-{
-    label: '试用期后',
-    value: 3
-}
+        label: '是',
+        value: 1
+    }, {
+        label: '否',
+        value: 2
+    },
+    {
+        label: '试用期后',
+        value: 3
+    }
 ]
 
 export const moneyTypeList = [{
-    label: '月薪',
-    value: 1
-}, {
-    label: '日薪',
-    value: 2
-},
-{
-    label: '时薪',
-    value: 3
-}
+        label: '月薪',
+        value: 1
+    }, {
+        label: '日薪',
+        value: 2
+    },
+    {
+        label: '时薪',
+        value: 3
+    }
 ]
 export const rewardTypeList = [{
-    label: '月返',
-    value: 1
-}, {
-    label: '日返',
-    value: 2
-},
-{
-    label: '时返',
-    value: 3
-},
-{
-    label: '一次性返',
-    value: 4
-}
+        label: '月返',
+        value: 1
+    }, {
+        label: '日返',
+        value: 2
+    },
+    {
+        label: '时返',
+        value: 3
+    },
+    {
+        label: '一次性返',
+        value: 4
+    }
 ]
 
 export const weekList = ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
