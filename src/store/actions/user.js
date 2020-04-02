dc4022deec17c3e8dcc32a7748cbf790477a198a
H5-ymm <1594363841@qq.com>
2020-04-02 16:20:04 +0800

    ymm

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -12,9 +12,21 @@ export const getAllUser = createAction(GETALLUSER, (data) => {
     return new Promise((resolve, reject) => {
         $http('/login/is_autologin', data).then(res => {
             wx.setStorageSync('phone', res.data.phone)
+            saveUser(res.data.uid)
             resolve(res.data);
         }).catch(error => {
             reject(error.status)
         })
     })
 })
+
+export const saveUser = uid => {
+    let userinfo = JSON.parse(wx.getStorageSync('wxInfo'))
+    let params = {
+        uid: uid,
+        head_img:userinfo.head_img
+      }
+    $http('/userinfo/editUserInfo', params).then(res => {
+      console.log(res)
+    })
+  }
